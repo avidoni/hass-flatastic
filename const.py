17094aef96408e181c05cfa76fd35acffd0aa158
avidoni <angela.vidoni@hotmail.ch>
2025-07-08 09:30:54 +0200

    initial commit

---
+++ const.py
@@ -0,0 +1,12 @@
+BASE_URL = "https://api.flatastic-app.com/index.php/api"
+HEADERS = {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "de-CH,de;q=0.9,en-US;q=0.8,en-CH;q=0.7,en;q=0.6,ar-JO;q=0.5,ar;q=0.4,de-DE;q=0.3",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "x-api-version": "2.0.0",
+    "x-client-version": "2.3.20"
+}
